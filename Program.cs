008f3d87b65393c131f8d1d5685bdbcfc3d23f60
agnlin02 <60339710+agnlin02@users.noreply.github.com>
2020-09-01 13:06:49 +0200

    Grej

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Programering_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
